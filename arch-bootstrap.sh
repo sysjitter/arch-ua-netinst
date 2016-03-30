59aa3c022131eac24ccab3259da9d75d99d99553
Harry Leitzell <harry@leitzell.org>
2016-03-30 14:05:22 -0500

    Cleanup white space and bootstrap a bit better with pacman-key

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -22,17 +22,15 @@ set -e -u -o pipefail
 
 # Packages needed by pacman (see get-pacman-dependencies.sh)
 PACMAN_PACKAGES=(
-  acl archlinux-keyring attr bzip2 curl expat glibc gpgme libarchive
-  libassuan libgpg-error libssh2 lzo openssl pacman pacman-mirrorlist xz zlib
-  krb5 e2fsprogs keyutils libidn gcc-libs
+acl archlinux-keyring attr bzip2 curl e2fsprogs expat gcc-libs glibc gpgme keyutils krb5 libarchive libassuan libgpg-error libidn libssh2 lzo openssl pacman pacman-mirrorlist xz zlib
 )
 BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem)
 EXTRA_PACKAGES=(coreutils bash grep gawk file tar systemd sed)
 DEFAULT_REPO_URL="https://mirrors.kernel.org/archlinux"
 DEFAULT_ARM_REPO_URL="https://archlinuxarm.org"
 
-stderr() { 
-  echo "$@" >&2 
+stderr() {
+  echo "$@" >&2
 }
 
 debug() {
@@ -49,14 +47,14 @@ fetch() {
 
 uncompress() {
   local FILEPATH=$1 DEST=$2
-  
+
   case "$FILEPATH" in
     *.gz) bsdtar xzf "$FILEPATH" -C "$DEST";;
     *.xz) bsdtar xJf "$FILEPATH" -C "$DEST";;
     *) debug "Error: unknown package format: $FILEPATH"
        return 1;;
   esac
-}  
+}
 
 ###
 get_default_repo() {
@@ -93,25 +91,25 @@ configure_pacman() {
 
 configure_minimal_system() {
   local DEST=$1
-  
+
   mkdir -p "$DEST/dev"
   echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd" 
   echo 'root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::' > "$DEST/etc/shadow"
   touch "$DEST/etc/group"
   echo "bootstrap" > "$DEST/etc/hostname"
-  
+
   test -e "$DEST/etc/mtab" || echo "rootfs / rootfs rw 0 0" > "$DEST/etc/mtab"
   test -e "$DEST/dev/null" || mknod "$DEST/dev/null" c 1 3
   test -e "$DEST/dev/random" || mknod -m 0644 "$DEST/dev/random" c 1 8
   test -e "$DEST/dev/urandom" || mknod -m 0644 "$DEST/dev/urandom" c 1 9
-  
+
   #sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$DEST/etc/pacman.conf"
   #sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$DEST/etc/pacman.conf"
 }
 
 fetch_packages_list() {
   local REPO=$1 
-  
+
   debug "fetch packages list: $REPO/"
   fetch "$REPO/" | extract_href | awk -F"/" '{print $NF}' | sort -rn ||
     { debug "Error: cannot fetch packages list: $REPO"; return 1; }
@@ -120,14 +118,18 @@ fetch_packages_list() {
 install_pacman_packages() {
   local BASIC_PACKAGES=$1 DEST=$2 LIST=$3 DOWNLOAD_DIR=$4
   debug "pacman package and dependencies: $BASIC_PACKAGES"
-  
+
   for PACKAGE in $BASIC_PACKAGES; do
     local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$")
     test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; return 1; }
     local FILEPATH="$DOWNLOAD_DIR/$FILE"
-    
-    debug "download package: $REPO/$FILE"
-    fetch -o "$FILEPATH" "$REPO/$FILE"
+
+    test "$FILEPATH" || \
+      debug "download package: $REPO/$FILE" && \
+      fetch -o "$FILEPATH" "$REPO/$FILE"
+    test "${FILEPATH}.sig" || \
+      debug "download package signature: $REPO/${FILE}.sig" && \
+      fetch -o "$FILEPATH.sig" "$REPO/${FILE}.sig"
     debug "uncompress package: $FILEPATH"
     uncompress "$FILEPATH" "$DEST"
   done
@@ -146,12 +148,16 @@ configure_static_qemu() {
 install_packages() {
   local ARCH=$1 DEST=$2 PACKAGES=$3
   debug "install packages: $PACKAGES"
+  LC_ALL=C chroot "$DEST" /usr/bin/pacman-key --init
+  LC_ALL=C chroot "$DEST" /usr/bin/pacman-key --populate archlinux
+  [[ "$ARCH" =~ ^arm.* ]] && LC_ALL=C chroot "$DEST" /usr/bin/pacman-key --populate archlinuxarm
+  LC_ALL=C chroot "$DEST" /usr/bin/pacman-key --refresh-keys
   LC_ALL=C chroot "$DEST" /usr/bin/pacman \
     --noconfirm --arch $ARCH -Sy --force $PACKAGES
 }
 
 show_usage() {
-  stderr "Usage: $(basename "$0") [-q] [-a i686|x86_64|arm|armv6h|armv7h|armv8] [-r REPO_URL] [-d DOWNLOAD_DIR] DESTDIR"
+  stderr "Usage: $(basename "$0") [-q] [-a i686|x86_64|arm|armv6h|armv7h|aarch64] [-r REPO_URL] [-d DOWNLOAD_DIR] DESTDIR"
 }
 
 main() {
@@ -161,7 +167,7 @@ main() {
   local REPO_URL=
   local USE_QEMU=
   local DOWNLOAD_DIR=
-  
+
   while getopts "qa:r:d:h" ARG; do
     case "$ARG" in
       a) ARCH=$OPTARG;;
@@ -173,10 +179,10 @@ main() {
   done
   shift $(($OPTIND-1))
   test $# -eq 1 || { show_usage; return 1; }
-  
+
   [[ -z "$ARCH" ]] && ARCH=$(uname -m)
   [[ -z "$REPO_URL" ]] && REPO_URL=$(get_default_repo "$ARCH")
-  
+
   local DEST=$1
   local REPO=$(get_core_repo_url "$REPO_URL" "$ARCH")
   [[ -z "$DOWNLOAD_DIR" ]] && DOWNLOAD_DIR=$(mktemp -d)
@@ -185,7 +191,7 @@ main() {
   debug "destination directory: $DEST"
   debug "core repository: $REPO"
   debug "temporary directory: $DOWNLOAD_DIR"
-  
+
   # Fetch packages, install system and do a minimal configuration
   mkdir -p "$DEST"
   [[ "$ARCH" =~ ^arm.* ]] && BASIC_PACKAGES=(${BASIC_PACKAGES[*]} archlinuxarm-keyring)
@@ -197,7 +203,7 @@ main() {
   install_packages "$ARCH" "$DEST" "${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}"
   configure_pacman "$DEST" "$ARCH" # Pacman must be re-configured
   [[ "$DOWNLOAD_DIR" ]] && rm -rf "$DOWNLOAD_DIR"
-  
+
   debug "done"
 }
 
