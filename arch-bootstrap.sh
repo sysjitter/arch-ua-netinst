8886f11b0284b9c794fae4b44fc53a2b51639bf6
Harry Leitzell <Harry.Leitzell@grainger.com>
2016-07-20 13:51:27 -0500

    Add in lz4 as a pacman dependency

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -22,7 +22,7 @@ set -e -u -o pipefail
 
 # Packages needed by pacman (see get-pacman-dependencies.sh)
 PACMAN_PACKAGES=(
-acl archlinux-keyring attr bzip2 curl e2fsprogs expat gcc-libs glibc gpgme keyutils krb5 libarchive libassuan libgpg-error libidn libssh2 lzo openssl pacman pacman-mirrorlist xz zlib
+acl archlinux-keyring attr bzip2 curl e2fsprogs expat gcc-libs glibc gpgme keyutils krb5 libarchive libassuan libgpg-error libidn libssh2 lzo openssl pacman pacman-mirrorlist xz zlib lz4
 )
 BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem)
 EXTRA_PACKAGES=(coreutils bash grep gawk file tar systemd sed)
